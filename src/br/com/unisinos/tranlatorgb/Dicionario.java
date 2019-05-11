755cea19b84e300b554e07e6ebca918cc3de7eea
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-05-10 21:27:04 -0300

    Adding file reading

--- src/br/com/unisinos/tranlatorgb/Dicionario.java
+++ src/br/com/unisinos/tranlatorgb/Dicionario.java
@@ -7,4 +7,9 @@ public class Dicionario {
     protected String palavra;
     protected List<String> definicoes;
 
+    public Dicionario(String palavra, List<String> definicoes) {
+        this.palavra = palavra;
+        this.definicoes = definicoes;
+    }
+
 }
--- src/br/com/unisinos/tranlatorgb/Tradutor.java
+++ src/br/com/unisinos/tranlatorgb/Tradutor.java
@@ -1,11 +1,39 @@
 package br.com.unisinos.tranlatorgb;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 public class Tradutor {
 
-    protected void carregaDicionario(String arq){}
+    protected void carregaDicionario(String arq){
+        try (BufferedReader br = new BufferedReader(new FileReader("dicionario.dat"))) {
+
+            Dicionario dicionario;
+            String linha, chave = "";
+            List<String> traducoes = Collections.emptyList();
+
+            while ((linha = br.readLine()) != null) {
+
+                String [] palavras = linha.split("#");
+
+                for (int i = 0; i < palavras.length; i++) {
+                    if(i == 0) {
+                        chave = palavras[i];
+                    }else {
+                        traducoes.add(palavras[i]);
+                    }
+                }
+
+                dicionario = new Dicionario(chave, traducoes);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public List traduzPalavra(String palavra){
         return Collections.emptyList();
