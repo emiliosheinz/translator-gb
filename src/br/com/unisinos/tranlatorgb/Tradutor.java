be8ca0a04d216b1901ee215772b996ce150c368c
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-05-11 09:28:38 -0300

    Fixing Tradutor class

--- src/br/com/unisinos/tranlatorgb/Tradutor.java
+++ src/br/com/unisinos/tranlatorgb/Tradutor.java
@@ -1,77 +1,74 @@
 package br.com.unisinos.tranlatorgb;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Tradutor {
 
-    protected void carregaDicionario(String arq){
+    protected void carregaDicionario(String arq) {
         try (BufferedReader br = new BufferedReader(new FileReader(arq))) {
 
             Dicionario dicionario;
-            String linha, chave = "";
-            List<String> traducoes = new ArrayList<>();
+            String linha;
+            List<Dicionario> palavrasDicionario = new ArrayList<>();
 
             while ((linha = br.readLine()) != null) {
 
-                String [] palavras = linha.split("#");
+                List<String> traducoes = new ArrayList<>();
+                String[] palavras = linha.split("#");
+                String chave = palavras[0];
 
-                for (int i = 0; i < palavras.length; i++) {
-                    if(i == 0) {
-                        chave = palavras[i];
-                    } else {
-                        traducoes.add(palavras[i]);
-                    }
+                for (int i = 1; i < palavras.length; i++) {
+                    traducoes.add(palavras[i]);
                 }
 
                 dicionario = new Dicionario(chave, traducoes);
+                palavrasDicionario.add(dicionario);
             }
+
+            System.out.println(palavrasDicionario);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-	public List<String> traduzPalavra(String palavra) {
-		return Collections.emptyList();
-	}
+    public List<String> traduzPalavra(String palavra) {
+        return Collections.emptyList();
+    }
 
-	public void insereTraducao(String palavra, List<String> definicoes) {
-		StringBuilder builder = new StringBuilder();
+    public void insereTraducao(String palavra, List<String> definicoes) {
+        StringBuilder builder = new StringBuilder();
 
-		builder.append(palavra + "#");
-		for (int i = 0; i < definicoes.size(); i++) {
-			if (i == definicoes.size() - 1) {
-				builder.append(definicoes.get(i));
-			} else {
-				builder.append(definicoes.get(i) + "#");
-			}
-		}
+        builder.append(palavra + "#");
+        for (int i = 0; i < definicoes.size(); i++) {
+            if (i == definicoes.size() - 1) {
+                builder.append(definicoes.get(i));
+            } else {
+                builder.append(definicoes.get(i) + "#");
+            }
+        }
 
-		File file = new File("dicionario.dat");
-		FileWriter fr = null;
+        File file = new File("dicionario.dat");
+        FileWriter fr = null;
 
-		try {
-			fr = new FileWriter(file);
-			fr.write(builder.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				fr.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        try {
+            fr = new FileWriter(file);
+            fr.write(builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public void salvaDicionario(String arq) {
-	}
+    public void salvaDicionario(String arq) {
+    }
 
 }
