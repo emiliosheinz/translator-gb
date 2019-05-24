94528f895983074bf65437c8665d41c4ea99946b
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-05-24 20:04:52 -0300

    Implementado o caso de fucking teste

--- src/br/com/unisinos/tranlatorgb/TradutorMain.java
+++ src/br/com/unisinos/tranlatorgb/TradutorMain.java
@@ -1,5 +1,12 @@
 package br.com.unisinos.tranlatorgb;
 
+import br.com.unisinos.tranlatorgb.enums.OrdemDeLeituraArvore;
+import br.com.unisinos.tranlatorgb.exceptions.PalavraNaoEncontradaException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TradutorMain {
 
     public static void main(String[] args) {
@@ -8,6 +15,26 @@ public class TradutorMain {
 
         tradutor.carregaDicionario("dicionario.dat");
 
-        System.out.println("dsf");
+        List<String> traducoesSwitch= new ArrayList<>();
+        traducoesSwitch.add("trocar");
+        traducoesSwitch.add("ecolher");
+
+        List<String> novasTraducoes = new ArrayList<>();
+        novasTraducoes.add("video-game");
+        novasTraducoes.add("trocar");
+
+        tradutor.insereTraducao("switch", traducoesSwitch);
+        tradutor.insereTraducao("switch", novasTraducoes);
+        try{
+            List<String> traducoes = tradutor.traduzPalavra("switch");
+            tradutor.traduzPalavra("xalala");
+            System.out.print("breakpoint");
+        }catch(PalavraNaoEncontradaException pne) {
+            System.out.println(pne.getMensagem());
+        }
+
+        tradutor.salvaDicionario("dicionario.dat", OrdemDeLeituraArvore.EM_ORDERM);
+
+        System.out.println("breakpoint");
     }
 }
